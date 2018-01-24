15c4e20ad49b3419cfe3a0ea1e890dacb7c6e4e2
Tito Valiente <titolvaliente@gmail.com>
2018-01-24 16:11:31 -0600

    updates to services branch

--- src/main/java/com/titovaliente/springboootblog/Post.java
+++ src/main/java/com/titovaliente/springboootblog/Post.java
@@ -1,7 +1,14 @@
 package com.titovaliente.springboootblog;
 
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
 public class Post {
+    @Id
+    private long id;
     private String title;
     private String body;
 
